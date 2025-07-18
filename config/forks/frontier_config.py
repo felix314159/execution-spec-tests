5b47493435f9e5f337b464caeb7a53486678a05f
Felix H <felix314159@users.noreply.github.com>
2025-07-18 14:25:25 +0000

    fix

--- config/forks/frontier_config.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class FrontierConfig(BaseModel):
-    gas_limit: int = 100
-    value_that_never_changed_after_frontier: int = 2
--- config/forks/homestead_config.py
+++
@@ -1,5 +0,0 @@
-from config.forks.frontier_config import FrontierConfig
-
-
-class HomesteadConfig(FrontierConfig):
-    gas_limit: int = 200
